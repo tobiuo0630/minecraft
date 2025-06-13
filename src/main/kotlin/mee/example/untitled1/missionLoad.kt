ba6b65e639602aa45b325c39d413fe539c820771
tobiuo <kaisei0000000@gmail.com>
2025-06-13 13:10:45 +0900

    スコープ関数がわからん

--- src/main/kotlin/mee/example/untitled1/missionLoad.kt
+++ src/main/kotlin/mee/example/untitled1/missionLoad.kt
@@ -1,4 +1,15 @@
 package mee.example.untitled1
 
+import com.google.gson.Gson
+import com.google.gson.reflect.TypeToken
+import java.io.File
+
 object missionLoad {
+    private val gson = Gson()
+
+    fun loadFile(file: File): List<missionData>{
+        if(!file.exists()) return emptyList()//.exists()はファイルがあるか確認する　emptyList()は要素が空のリスト
+        val type = object : TypeToken<List<missionData>>() {}.type
+        return file.reader().use{ gson.fromJson(it,type)//useが拡張関数
+    }
 }
